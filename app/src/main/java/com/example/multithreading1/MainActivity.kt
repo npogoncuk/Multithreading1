df7c7c22a0c5bf26a71e85ef2a1d5cd994f6635e
npogoncuk <npogoncuk@gmail.com>
2022-08-15 15:46:21 +0300

    Implemented saving data into a file on internal storage

--- app/src/main/java/com/example/multithreading1/MainActivity.kt
+++ app/src/main/java/com/example/multithreading1/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.multithreading1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.multithreading1.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private const val FILE_NAME = "userData.txt"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveDataButton.setOnClickListener {
+            suspend fun writeFileOnInternalStorage() {
+                withContext(Dispatchers.IO) {
+                    val text = with(binding) {
+                        inputLayoutName.editText?.text.toString() + "\n" +
+                                inputLayoutSurname.editText?.text.toString() + "\n" +
+                                inputLayoutEmail.editText?.text.toString() + "\n" +
+                                inputLayoutPhone.editText?.text.toString()
+                    }
+                    with(openFileOutput(FILE_NAME, MODE_PRIVATE)) {
+                        write(text.toByteArray())
+                        flush()
+                        close()
+                    }
+                }
+            }
+            lifecycleScope.launch {
+                writeFileOnInternalStorage()
+            }
+        }
+
+
     }
 }
