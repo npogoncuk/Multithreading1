9966924bed9c20f30e60b45ccbdca08607843124
npogoncuk <npogoncuk@gmail.com>
2022-08-15 17:16:17 +0300

    Refactoring

--- app/src/main/java/com/example/multithreading1/MainActivity.kt
+++ app/src/main/java/com/example/multithreading1/MainActivity.kt
@@ -4,13 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.example.multithreading1.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.io.FileNotFoundException
-import java.util.NoSuchElementException
 
 private const val FILE_NAME = "userData.txt"
 const val SAVED_DATA_KEY = "saved_data"
@@ -25,42 +21,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.saveDataButton.setOnClickListener {
-            suspend fun writeFileOnInternalStorage() {
-                withContext(Dispatchers.IO) {
-                    val text = with(binding) {
-                        inputLayoutName.editText?.text.toString().ifBlank { "You saved blank text" } + "\n" +
-                                inputLayoutSurname.editText?.text.toString().ifBlank { "You saved blank text" } + "\n" +
-                                inputLayoutEmail.editText?.text.toString().ifBlank { "You saved blank text" } + "\n" +
-                                inputLayoutPhone.editText?.text.toString().ifBlank { "You saved blank text" }
-                    }
-                    with(openFileOutput(FILE_NAME, MODE_PRIVATE)) {
-                        write(text.toByteArray())
-                        flush()
-                        close()
-                    }
-                }
-            }
-            lifecycleScope.launch {
+            CoroutineScope(Dispatchers.IO).launch {
                 writeFileOnInternalStorage()
             }
         }
 
         binding.seeDataButton.setOnClickListener {
-            suspend fun readDataFromFile(): String? {
-                return withContext(Dispatchers.IO) {
-                    var data: List<String>?
-                    try {
-                        data = filesDir.listFiles()?.first { it.name == FILE_NAME }?.readLines()
-                    } catch (e: FileNotFoundException) {data = null}
-                    catch (e: NoSuchElementException) {data = null}
-
-                    if (data != null) {
-                        data.map { it.ifBlank { "No data" } }
-                        return@withContext resources.getString(R.string.saved_data, data[0], data[1], data[2], data[3])
-                    } else null
-                }
-            }
-            lifecycleScope.launch {
+            CoroutineScope(Dispatchers.IO).launch {
                 val savedData = readDataFromFile()
                 if (savedData != null) {
                     val intent = Intent(this@MainActivity, SeeDataActivity::class.java)
@@ -70,6 +37,41 @@ class MainActivity : AppCompatActivity() {
                 else Toast.makeText(this@MainActivity, "You should save file first", Toast.LENGTH_SHORT).show()
             }
 
+        }
+    }
+
+    private suspend fun writeFileOnInternalStorage() {
+        withContext(Dispatchers.IO) {
+            val text = with(binding) {
+                inputLayoutName.editText?.text.toString().ifBlank { "You saved blank text" } + "\n" +
+                        inputLayoutSurname.editText?.text.toString().ifBlank { "You saved blank text" } + "\n" +
+                        inputLayoutEmail.editText?.text.toString().ifBlank { "You saved blank text" } + "\n" +
+                        inputLayoutPhone.editText?.text.toString().ifBlank { "You saved blank text" }
+            }
+            kotlin.runCatching {
+                with(openFileOutput(FILE_NAME, MODE_PRIVATE)) {
+                    write(text.toByteArray())
+                    flush()
+                    close()
+                }
+            }
+        }
+    }
+
+    private suspend fun readDataFromFile(): String? {
+        return withContext(Dispatchers.IO) {
+            val data: List<String>? = try {
+                filesDir.listFiles()?.first { it.name == FILE_NAME }?.readLines()
+            } catch (e: FileNotFoundException) {
+                null
+            } catch (e: NoSuchElementException) {
+                null
+            }
+
+            if (data != null) {
+                data.map { it.ifBlank { "No data" } }
+                return@withContext resources.getString(R.string.saved_data, data[0], data[1], data[2], data[3])
+            } else null
         }
     }
 }
